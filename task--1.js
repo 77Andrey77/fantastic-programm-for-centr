86bb0273765cc8c0e625086e59da4b3bf8a3ecc3
Andrii Pysmennyi <andrey.ad511989@gmail.com>
2021-10-11 13:38:31 +0300

    updates style and logic

--- task--1.js
+++ task--1.js
@@ -1,6 +1,6 @@
 // для левой горизонтальной координты
-let inputLEl = document.querySelector('#G-left');
-let outputLEl = document.querySelector('#outputL');
+let inputLEl = document.querySelector('#G-left');  //ввод левой координаты
+let outputLEl = document.querySelector('#outputL');  //вывод левойкоординаты
 
 inputLEl.oninput = function () {
     if (inputLEl.value === '') {
@@ -10,9 +10,9 @@ inputLEl.oninput = function () {
 
 // для правой горизонтальной координты
 
-let inputREl = document.querySelector('#G-right');
-let outputREl = document.querySelector('#outputR');
-let outputLuft = document.querySelector('#luft');
+let inputREl = document.querySelector('#G-right');   //ввод правой координаты
+let outputREl = document.querySelector('#outputR');   //вывод правой координаты
+let outputLuft = document.querySelector('#luft');     // вывод люфт 
 
 
 inputREl.oninput = function () {
@@ -20,14 +20,16 @@ inputREl.oninput = function () {
         outputREl.textContent = 0
     } else if (Number(inputLEl.value) > Number(inputREl.value)) {
         outputREl.textContent = inputREl.value;
-        outputLuft.textContent=Math.abs(Number.parseFloat(outputREl.textContent)-Number.parseFloat(outputLEl.textContent)) ;
+
+        outputLuft.textContent=Math.abs((Number(outputREl.textContent)*1000-Number(outputLEl.textContent)*1000)/1000) ;
     } else if (Number(inputLEl.value) < Number(inputREl.value)) {
         outputREl.textContent = inputREl.value;
         
-        outputLuft.textContent=Math.abs(Number.parseFloat(outputLEl.textContent)-Number.parseFloat(outputREl.textContent)) ; 
+        outputLuft.textContent=Math.abs((Number(outputLEl.textContent)*1000-Number(outputREl.textContent)*1000)/1000) ; 
     } else {
         outputREl.textContent = inputREl.value;
-        outputLuft.textContent =Math.abs(Number.parseFloat(outputLEl.textContent) - Number.parseFloat(outputREl.textContent)) ;
+
+        outputLuft.textContent =Math.abs((Number(outputLEl.textContent)*1000 - Number(outputREl.textContent)*1000)/1000) ;
     }
     //{
        // outputREl.textContent = inputREl.value;
@@ -36,30 +38,46 @@ inputREl.oninput = function () {
        // } }
 }
 // для вертикальной координты
-let inputVEl = document.querySelector('#Vertikal');
-let outputVEl = document.querySelector('#outputV');
+let inputVEl = document.querySelector('#Vertikal');   //ввод вертикальной координаты
+let outputVEl = document.querySelector('#outputV');    //вывод вертикальной координаты
 
 inputVEl.oninput = function () {
     if (inputVEl.value === '') {
         outputVEl.textContent = 0
     } else { outputVEl.textContent = inputVEl.value; }
 }
-//выступание - западание
-let inputLLedgeEl = document.querySelector('#LLedge');
-let outputLLedgeEl = document.querySelector('#outputLLedge');
+//////////////////////выступание - западание//////////////////////
+//левая координата
+let leftLedge;
+let rightLedge;
+let inputLLedgeEl = document.querySelector('#LLedge');          //ввод левого выступания
+let outputLLedgeEl = document.querySelector('#outputLLedge');     //вывод левого выст
+//////
+let outputLLedgeElClass = document.querySelector('.outputLLedge');
+//////
 inputLLedgeEl.oninput = function () {
     outputLLedgeEl.textContent = inputLLedgeEl.value;
+    leftLedge = outputLLedgeEl.textContent;
+console.log(leftLedge);
 }
-//////////
-let inputRLedgeEl = document.querySelector('#RLedge');
-let outputRLedgeEl = document.querySelector('#outputRLedge');
+////////////правая координата
+let inputRLedgeEl = document.querySelector('#RLedge');                //ввод правого выступ
+let outputRLedgeEl = document.querySelector('#outputRLedge');         //вывод правого выст
+//////
+let outputRLedgeElClass = document.querySelector('.outputRLedge');
+//////
 inputRLedgeEl.oninput = function () {
     outputRLedgeEl.textContent = inputRLedgeEl.value;
+    rightLedge = outputRLedgeEl.textContent;
+console.log(rightLedge);
+    ////смена цвета
+    if (leftLedge === rightLedge) {
+        outputLLedgeElClass.classList.add('output-ok');
+        outputRLedgeElClass.classList.add('output-ok');
+    } else {
+        outputLLedgeElClass.classList.remove('output-ok');
+        outputRLedgeElClass.classList.remove('output-ok'); }
 }
-//////////////проверка выступани западания
-//if (Number(outputRLedgeEl.textContent) === Number(outputLLedgeEl.textContent)) {}
-
-
 ///////////////////////////////////////////////////////////
 
 //делаем перемещение для изменения значений координаt
@@ -72,42 +90,42 @@ let inputRightDownEl = document.querySelector('#down-right');
 
 inputLeftUpEl.oninput = function () {
     if (inputLeftUpEl.value === "") {
-        outputLEl.textContent= Number.parseFloat(inputLEl.value) + 0
+        outputLEl.textContent= Number(inputLEl.value) + 0
     } else {
-        outputLEl.textContent = Number.parseFloat(inputLEl.value) + Number.parseFloat(inputLeftUpEl.value) / 2
-        outputVEl.textContent = Number.parseFloat(inputVEl.value) + Number(inputLeftUpEl.value) / 2
-        outputLLedgeEl.textContent = Number.parseFloat(inputLLedgeEl.value)+ Number.parseFloat(inputLeftUpEl.value)
+        outputLEl.textContent = (Number(inputLEl.value)*1000 + Number(inputLeftUpEl.value) / 2*1000)/1000
+        outputVEl.textContent = (Number(inputVEl.value)*1000 + Number(inputLeftUpEl.value) / 2*1000)/1000
+        outputLLedgeEl.textContent = (Number(inputLLedgeEl.value)*1000+ Number(inputLeftUpEl.value)*1000)/1000
     }
 }
 
 inputRightUpEl.oninput = function () {
     if (inputRightUpEl.value === "") {
-        outputREl.textContent= Number.parseFloat(inputREl.value)+0
+        outputREl.textContent= Number(inputREl.value)+0
     } else if (inputRightUpEl.value === inputLeftUpEl.value) {
-        outputLEl.textContent = Number.parseFloat(inputLEl.value)
-        outputREl.textContent= Number.parseFloat(inputREl.value)
-        outputVEl.textContent = Number.parseFloat(outputVEl.textContent) - Number.parseFloat(inputRightUpEl.value) / 2 + Number(inputRightUpEl.value)
-        outputRLedgeEl.textContent = Number.parseFloat(inputRLedgeEl.value)+Number.parseFloat(inputRightUpEl.value)
+        outputLEl.textContent = Number(inputLEl.value)
+        outputREl.textContent= Number(inputREl.value)
+        outputVEl.textContent = Number(outputVEl.textContent) - Number(inputRightUpEl.value) / 2 + Number(inputRightUpEl.value)
+        outputRLedgeEl.textContent = (Number(inputRLedgeEl.value)*1000+Number(inputRightUpEl.value)*1000)/1000
     }
     else {
-        outputREl.textContent = Number.parseFloat(inputREl.value) + Number.parseFloat(inputRightUpEl.value) / 2
-        outputVEl.textContent = Number.parseFloat(outputVEl.textContent) + Number.parseFloat(inputRightUpEl.value) / 2
-        outputRLedgeEl.textContent = Number.parseFloat(inputRLedgeEl.value)+Number.parseFloat(inputRightUpEl.value)
+        outputREl.textContent = (Number(inputREl.value)*1000 + Number(inputRightUpEl.value)*1000 )/ 2000
+        outputVEl.textContent = (Number(outputVEl.textContent)*1000 + Number(inputRightUpEl.value)*1000 )/ 2000
+        outputRLedgeEl.textContent = (Number(inputRLedgeEl.value)*1000+ Number(inputRightUpEl.value)*1000)/1000
     }
 }
 
 inputLeftDownEl.oninput = function () {
     if (inputLeftDownEl === "") {
-        outputLEl.textContent= Number.parseFloat(inputLEl.value) + 0
+        outputLEl.textContent= Number(inputLEl.value) + 0
     } else {
-       outputLEl.textContent = Number.parseFloat(outputLEl.textContent) + Number.parseFloat(inputLeftDownEl.value)
+       outputLEl.textContent = (Number(outputLEl.textContent)*1000 + Number(inputLeftDownEl.value)*1000)/1000
     }
 }
 
 inputRightDownEl.oninput = function () {
     if (inputRightDownEl === "") {
-        outputREl.textContent= Number.parseFloat(inputREl.value) + 0
+        outputREl.textContent= Number(inputREl.value) + 0
     } else {
-        outputREl.textContent= Number.parseFloat(outputREl.textContent)+ Number.parseFloat(inputRightDownEl.value)
+        outputREl.textContent= (Number(outputREl.textContent)*1000+ Number(inputRightDownEl.value)*1000)/1000
     }
 }
